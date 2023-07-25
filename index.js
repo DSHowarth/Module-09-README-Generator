7098237b64683603b49cab83c59afaa8a417ab16
Dash <dkshowarth@gmail.com>
2023-07-24 21:01:49 -0700

    add README template

---
+++ index.js
@@ -0,0 +1,33 @@
+const template = `
+#${title}
+
+## Description
+
+${desc}
+
+## Installation
+
+${installInstr}
+
+## Usage
+
+${usageInstr}
+
+## Contributing
+
+${contributingInstr}
+
+## Tests
+
+${testInstr}
+
+## License
+
+${license} 
+
+## Questions
+
+This repo was created and maintained by https://github.com/${userName}. 
+
+Questions? Contact them at ${email}
+`
